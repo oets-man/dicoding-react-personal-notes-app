ef1573370c5765f9f368737c063355849bfd5c13
Usman Husnan <oets.man@gmail.com>
2023-09-24 17:19:31 +0700

    .

--- src/note/ListContainer.jsx
+++ src/note/ListContainer.jsx
@@ -6,15 +6,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 export default function ListContainer({ notes, toggleArchived, view }) {
-	let localNotes = notes;
-	if (view == 'isArchived') localNotes = notes.filter((note) => note.archived == true);
-	if (view == 'isNotArchived') localNotes = notes.filter((note) => note.archived == false);
-
-	// console.log(localNotes);
+	let filteredNotes = notes;
+	if (view == 'isArchived') filteredNotes = notes.filter((note) => note.archived == true);
+	if (view == 'isNotArchived') filteredNotes = notes.filter((note) => note.archived == false);
 
 	return (
 		<Row>
-			{localNotes.map((note) => (
+			{filteredNotes.map((note) => (
 				<Col sm="12" md="6" lg="4" key={note.id} className="p-2">
 					<ListItems {...note} toggleArchived={toggleArchived} />
 				</Col>
