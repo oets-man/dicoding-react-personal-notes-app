26d4d15743dbc074d80df4eb02682bf5ff90b90f
Usman Husnan <oets.man@gmail.com>
2023-09-24 17:05:56 +0700

    success change-view

--- src/note/ListContainer.jsx
+++ src/note/ListContainer.jsx
@@ -2,15 +2,23 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import ListItems from './ListItems';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-export default function ListContainer({ notes }) {
+export default function ListContainer({ notes, toggleArchived, view }) {
+	let localNotes = notes;
+	if (view == 'isArchived') localNotes = notes.filter((note) => note.archived == true);
+	if (view == 'isNotArchived') localNotes = notes.filter((note) => note.archived == false);
+
+	// console.log(localNotes);
+
 	return (
-		<div className='row'>
-			{notes.map((note) => (
-				<div className='col-12 col-md-6 col-xl-4' key={note.id}>
-					<ListItems  {...note} />
-				</div>
+		<Row>
+			{localNotes.map((note) => (
+				<Col sm="12" md="6" lg="4" key={note.id} className="p-2">
+					<ListItems {...note} toggleArchived={toggleArchived} />
+				</Col>
 			))}
-		</div>
+		</Row>
 	);
 }
--- src/note/ListItems.jsx
+++ src/note/ListItems.jsx
@@ -2,17 +2,30 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import showFormattedDate from '../utils/format-date';
-import { Button } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 
-
-export default function ListItems({ title, body, createdAt }) {
+export default function ListItems({ title, body, createdAt, archived, toggleArchived, id }) {
 	return (
-		<>
-      <Button variant="primary">Primary</Button>{' '}
-
-			<h2>{title}</h2>
-			<p>{body}</p>
-			<p>{showFormattedDate(createdAt)}</p>
-		</>
+		<Card className="p-3">
+			<Card.Title className="row">
+				<h2 className="col-auto fw-normal fs-4">{title}</h2>
+				<div className="col text-end">
+					<Form.Check // prettier-ignore
+						type="switch"
+						key={archived}
+						checked={archived}
+						onChange={() => toggleArchived(id)}
+					/>
+					<p className="small fw-light fs-6 m-0">Arsipkan!</p>
+				</div>
+			</Card.Title>
+			<Card.Body className="p-0">
+				{/* <p>{archived ? 'ya' : 'tidak'}</p> */}
+				<p>{body}</p>
+				<hr />
+				<p className="m-0">{showFormattedDate(createdAt)}</p>
+			</Card.Body>
+		</Card>
 	);
 }
